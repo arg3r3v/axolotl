78b25af3f499ab6386b1400801e1c6e5e4ece219
arg3r3v <124920442+arg3r3v@users.noreply.github.com>
2024-06-13 13:35:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,40 @@ document.addEventListener('DOMContentLoaded', () => {
         updateCarousel();
     }, 5000); // Change image every 5 seconds
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const closeBtn = document.querySelector('.close-btn');
+    const fixedIcon = document.querySelector('.fixed-icon');
+
+    const closeBtnLeft = document.querySelector('.close-btn-left');
+    const fixedIconLeft = document.querySelector('.fixed-icon-left');
+    const popupIcon = document.getElementById('popup-icon');
+    const popup = document.getElementById('popup');
+    const popupClose = document.querySelector('.popup-close');
+
+    closeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        fixedIcon.style.display = 'none';
+    });
+
+    closeBtnLeft.addEventListener('click', (e) => {
+        e.preventDefault();
+        fixedIconLeft.style.display = 'none';
+    });
+
+    popupIcon.addEventListener('click', (e) => {
+        e.preventDefault();
+        popup.style.display = 'flex';
+    });
+
+    popupClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+
+    // Cerrar el popup al hacer clic fuera de su contenido
+    window.addEventListener('click', (e) => {
+        if (e.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
